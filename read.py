e15fe2a0666d5f5c02b079a2c85b0757f386da82
Vladimir <assassin0506@rambler.ru>
2023-04-24 09:00:09 +0300

    new script

--- read.py
+++ read.py
@@ -1,16 +1,31 @@
-
+import numpy as np
 import cv2.cv2 as cv
 import matplotlib.pyplot as plt
 
-img = cv.imread('sh1.jpg')
+img = cv.imread('sh2.jpg')
 img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
 gray = cv.cvtColor(img, cv.COLOR_RGBA2GRAY)
 
-_, binary = cv.threshold(gray, 205, 255, cv.THRESH_BINARY)
-#plt.imshow(binary, cmap="gray")
-#plt.show()
+_, binary = cv.threshold(gray, 170, 255, cv.THRESH_BINARY)
+plt.imshow(binary, cmap="gray")
+plt.show()
 
-contours, hierarchy = cv.findContours(binary, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+contours, hierarchy = cv.findContours(binary, cv.RETR_TREE, cv.CHAIN_APPROX_TC89_L1)
 image = cv.drawContours(img, contours, -1, (0, 255, 0), 2)
 plt.imshow(img)
 plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
