da5028ea931fe57dc04fa2d72861a66b25b755e0
Vladimir <assassin0506@rambler.ru>
2023-05-03 06:17:50 +0300

    erosion

--- script1.py
+++ script1.py
@@ -1,40 +1,63 @@
-import numpy as np
 import argparse
+import cv2
 import imutils
-import cv2 as cv
+import numpy as np
 
 ap = argparse.ArgumentParser()
-ap.add_argument('-i', '--image', type=str, default='sh2.jpg', help="path to img")
+ap.add_argument("-i", "--image", type=str, default="sh2.jpg", help="path to img")
 args = vars(ap.parse_args())
 
-image = cv.imread(args['image'])
-cv.imshow('Image', image)
+# Read and display original image
+image = cv2.imread(args["image"])
+cv2.imshow("Original", image)
 
-gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-thresh = cv.threshold(gray, 160, 255, cv.THRESH_BINARY_INV) [1]
-cv.imshow("Thresh", thresh)
+# Apply grayscase color pallete and display image
+img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+cv2.imshow("Grayscale", img_gray)
 
-cnts = cv.findContours(thresh.copy(), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-cnts = imutils.grab_contours(cnts)
-c = max(cnts, key=cv.contourArea)
+# Apply threshold and display image
+img_threshold = cv2.threshold(img_gray, 145, 255, cv2.THRESH_BINARY_INV)[1]
+img_threshold = cv2.bitwise_not(img_threshold)
+cv2.imshow("Thresh", img_threshold)
 
-output = image.copy()
-cv.drawContours(output, [c], -1, (0, 255, 0), 3)
-(x, y, w, h) = cv.boundingRect(c)
+#Threshold erosion
+kernel = np.ones((3, 3), np.uint8)
+img_erosion = cv2.erode(img_threshold, kernel, iterations=3)
+img_dilation = cv2.dilate(img_erosion, kernel, iterations=2)
+cv2.imshow("ErodedDilatedImg", img_dilation)
+
+#testVer erosion
+#opening = cv2.morphologyEx(img_threshold, cv2.MORPH_OPEN, kernel)
+#cv2.imshow("123", opening)
+
+
+# Find contours and display image
+img_contours = image.copy()
+contours, hierarchy = cv2.findContours(
+    img_dilation, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE
+)
+cnts = imutils.grab_contours((contours, hierarchy))
+c = max(cnts, key=cv2.contourArea)
+cv2.drawContours(img_contours, [c], -1, (0, 255, 0), 3)
+(x, y, w, h) = cv2.boundingRect(c)
 text = "original, num_pts={}".format(len(c))
-cv.putText(output, text, (x, y - 15), cv.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+cv2.putText(
+    img_contours, text, (x, y - 15), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2
+)
 
 print("[INFO] {}".format(text))
-cv.imshow("Original Contour", output)
-cv.waitKey(0)
+cv2.imshow("Original Contour", img_contours)
+cv2.waitKey(0)
 
 for eps in np.linspace(0.001, 0.05, 10):
-	peri = cv.arcLength(c, True)
-	approx = cv.approxPolyDP(c, eps * peri, True)
-	output = image.copy()
-	cv.drawContours(output, [approx], -1, (0, 255, 0), 3)
-	text = "eps={:.4f}, num_pts={}".format(eps, len(approx))
-	cv.putText(output, text, (x, y - 15), cv.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
-	print("[INFO] {}".format(text))
-	cv.imshow("Approximated Contour", output)
-	cv.waitKey(0)
+    peri = cv2.arcLength(c, True)
+    approx = cv2.approxPolyDP(c, eps * peri, True)
+    output = image.copy()
+    cv2.drawContours(output, [approx], -1, (0, 255, 0), 3)
+    text = "eps={:.4f}, num_pts={}".format(eps, len(approx))
+    cv2.putText(
+        output, text, (x, y - 15), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2
+    )
+    print("[INFO] {}".format(text))
+    cv2.imshow("Approximated Contour", output)
+    cv2.waitKey(0)
